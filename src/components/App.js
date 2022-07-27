7f51cd27f394abed352a12105058dc2000b47d34
pablo-lucas <pablo.lucv@gmail.com>
2022-07-26 19:24:45 -0500

    Added new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+  onTermSubmit = async (term) => {
+    const reponse = await youtube.get("/search", {
       params: {
-        q: term
-      }
-    })
-  }
- 
+        q: term,
+      },
+    });
+
+    this.setState({ videos: reponse.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
